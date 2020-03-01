18e802f2ef59e76412e1ee4e8582540c7191a1b7
k1012g <kuri.ladybaby1012@gmail.com>
2020-03-01 23:02:02 +0900

    printf practice done

---
+++ 1/List/1-4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+	printf("こんにちは。\n私の名前は田中一郎です。\n");
+
+	return 0;
+}
---
+++ 1/List/1-5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+	printf("こんにちは。\n");
+	printf("私の名前はEfren Rayesです。\n");
+
+	return 0;
+}
