d24f25da3cbea4da0027f53e54e7d30944a9196c
k1012g <kuri.ladybaby1012@gmail.com>
2020-03-03 16:57:36 +0900

    chapter3 done

---
+++ 3/List/3-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void){
+	int num;
+
+	puts("整数を入力してください。");
+
+	printf("整数 : "); scanf("%d", &num);
+
+	switch (num % 3) {
+		case 0:
+			puts("その数は3で割り切れます。");
+			break;
+		case 1:
+			puts("その数を3で割った剰余は1です。");
+			break;
+		case 2:
+			puts("その数を3で割った剰余は2です。");
+			break;
+	}
+
+	return 0;
+}
